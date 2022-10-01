0daab67a01032de09e512c6c5b994ce26e46dd96
Isaac Yang <isaacy@nvidia.com>
2022-09-30 17:22:59 -0700

    Add more pydantic schemas

--- nvflexible/app/access/schemas.py
+++ nvflexible/app/access/schemas.py
@@ -1,20 +1,32 @@
 from typing import Union
+from xmlrpc.client import DateTime
 
 from pydantic import BaseModel
+
+class SubmissionCustomField(BaseModel):
+    id : int
+    key_name : str
+    value_type : str
+    value_string : str
 
 
 class SubmissionBase(BaseModel):
     state : str = ""
-
+    custom_fields : list[SubmissionCustomField] = []
 
 class SubmissionCreate(SubmissionBase):
-    pass
+    parents : list['Submission']
+
+    class Config:
+        orm_mode = True
 
 
 class Submission(SubmissionBase):
     id: int
     owner_id: int
-
+    parents : list['Submission']
+    created_at : DateTime
+    updated_at : DateTime
     class Config:
         orm_mode = True
 
